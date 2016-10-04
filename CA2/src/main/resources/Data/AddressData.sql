9e17544724144945d6bc96c1ef6647b2ccc4bf8b
Louise B. Nielsen <Louise B. Nielsen>
2016-10-04 11:00:53 +0200

    testdata

---
+++ CA2/src/main/resources/Data/AddressData.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  LouiseB
+ * Created: 04-10-2016
+ */
+
+INSERT INTO ADDRESS (STREET, ADDITIONALINFO) VALUES ('Nørgaardsvej', 'x');
+INSERT INTO ADDRESS (STREET, ADDITIONALINFO) VALUES ('Retskafsvej', 'x');
+INSERT INTO ADDRESS (STREET, ADDITIONALINFO) VALUES ('Engbovej', 'x');
