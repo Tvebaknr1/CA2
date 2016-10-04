64b54462ab3ae60a598f1a87ba977de649163e2f
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-10-04 14:25:37 +0200

    fixed merge errors

--- CA2/src/main/java/Facade/FacadePerson.java
+++ CA2/src/main/java/Facade/FacadePerson.java
@@ -50,7 +50,7 @@ public class FacadePerson {
 
         try {
             em.getTransaction().begin();
-            persons = em.createQuery("Select i from Person i").getResultList();
+            persons = em.createQuery("Select i from InfoEntity i").getResultList();
 
             em.getTransaction().commit();
             return persons;
